a570c5de231a6e7fbfdb1ae0c6dd4a2e28bf4388
Hung.Thai(Patrick) /Sotatek/DnU <e.hung.thai@wesang.com>
2024-08-23 15:36:11 +0700

    sharp

--- src/components/SendEmail.ts
+++ src/components/SendEmail.ts
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export const SendEmail = async (formdata: FormData) => {
     const message = formdata.get("message");
     const name = formdata.get("name");
-    const SenderEmail = formdata.get("SenderEmail");
+    const senderEmail = formdata.get("SenderEmail");
     if (!message) {
         return {
             error: "Invalid message",
@@ -15,7 +15,7 @@ export const SendEmail = async (formdata: FormData) => {
         from: "Contact Form <onboarding@resend.dev>",
         to: `hungdhv97@gmail.com`,
         subject: `${name} From Contact Form.`,
-        reply_to: `${SenderEmail}`,
+        reply_to: `${senderEmail}`,
         text: `${message}`,
     });
 
